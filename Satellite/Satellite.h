a81c8141113b75f6dc69bf06841dc990e876bd09
rayden96 <rayden.logan.viljoen@gmail.com>
2021-11-13 14:53:18 +0200

    Changes Saturday 13th Afternoon

--- Satellite/Satellite.h
+++ Satellite/Satellite.h
@@ -1,8 +1,11 @@
 #ifndef SATELLITE_H
 #define SATELLITE_H
-
+//Remember to add trajectory calculator
 #include <iostream>
 #include <string>
+#include "Department.h"
+#include "GroundControl.h"
+#include "SatelliteFunctionality.h"
 
 using namespace std;
 
@@ -12,6 +15,29 @@ class Satellite {
         bool attached;
         Department *currentDepartment;
         int xPos, yPos, zPos;
+        SatelliteFunctionality *strategy;
+        GroundControl *GroundControlDep;
+    
+    public:
+        Satellite(double c);
+        ~Satellite();
+        void doFunctionality();
+
+
+        //getters
+        double getCost();
+        bool getAttached();
+        int getX();
+        int getY();
+        int getZ();
+
+        //setters
+        void setCost(double x);
+        void setAttached(bool b);
+        void setX(int x);
+        void setY(int y);
+        void setZ(int z);
+
 
 };
 
