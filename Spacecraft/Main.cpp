5fd90659ac0483349b44ba2842e2e639de867571
Agape <theaman249@gmail.com>
2021-11-16 14:35:23 +0200

    Created a Main.cpp for us to test anything related to the spaceCraft

---
+++ Spacecraft/Main.cpp
@@ -0,0 +1,18 @@
+#include "string"
+#include <iostream>
+
+//spaceCraft
+#include "Dragon.h"
+#include "CrewDragon.h"
+#include "SpaceShuttle.h"
+
+using namespace std;
+
+int main() {
+
+
+    //This is a test for how the space shuttle will interact with the Rocket
+
+
+    return 0;
+}
