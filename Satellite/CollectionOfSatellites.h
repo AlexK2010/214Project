3503346e65cf1220a9193aee57f8e8508abf20f2
rayden96 <rayden.logan.viljoen@gmail.com>
2021-11-16 13:13:12 +0200

    Some upgrades

---
+++ Satellite/CollectionOfSatellites.h
@@ -0,0 +1,15 @@
+#ifndef COLLECTIONOFSATELLITES_H
+#define COLLECTIONOFSATELLITES_H
+
+#include "Satellite.h"
+#include "SatelliteIterator.h"
+
+class CollectionOfSatellites{
+        Satellite* start;
+    public:
+        SatelliteIterator* createSatelliteIterator();
+        void addSatellite(Satellite*); // add from back
+        void removeSatellite(); //remove from back
+        bool isEmpty();
+};
+#endif
