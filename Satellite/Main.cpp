c2cae119d89e3f3711e7ae46d2e491b550432fe2
aryanramnand <aryanramnand@gmail.com>
2021-11-18 09:34:30 +0200

    Added Main base 2021/11/18 09:34

---
+++ Satellite/Main.cpp
@@ -0,0 +1,27 @@
+#include "string"
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+#include "Satellite.h"
+#include "Department.h"
+#include "GroundControl.h"
+#include "Path.h"
+
+#include "ChangeTrajectory.h"
+#include "CollectionOfSatellites.h"
+#include "MaintenanceDepartment.h"
+#include "RiskAnalysis.h"
+#include "SatelliteFunctionality.h"
+#include "SatelliteIterator.h"
+#include "SignalDepartment.h"
+#include "ToggleSignal.h"
+#include "TrajectoryDepartment.h"
+
+using namespace std;
+
+int main() {
+
+
+    return 0;
+}
