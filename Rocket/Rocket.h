1afbc0e9cec5ea2b5ee2b61d520ec10ece8367e9
Agape <theaman249@gmail.com>
2021-11-18 20:23:15 +0200

    Just added some methods needed by the SpaceShuttle

--- Rocket/Rocket.h
+++ Rocket/Rocket.h
@@ -1,11 +1,21 @@
 #ifndef ROCKET_H
 #define ROCKET_H
 
+//Rocket
 #include "EOIterator.h"
 #include "RocketObserver.h"
 #include "RocketStage.h"
 #include "RocketFirstStage.h"
 #include "StageCreator.h"
+
+//SpaceCraft
+#include "../Spacecraft/SpaceShuttle.h"
+
+//Satellites
+
+class SpaceShuttle; //forwrad Declarations of Space Shuttle
+
+
 
 using namespace std;
 
@@ -18,10 +28,32 @@ class Rocket
         int numEnginesFailed;       //Number of engines that have failed
         int carryWeight;            //Carry weight of the rocket
         int attachedWeight;         //Weight attached to the rocket
+
+        /*
+            THIS STUFF IS GOING TO BE USED BY THE SPACECRAFT. PLEASE CONSULT BEFORE TOUCHING
+        */
+
+        bool RocketStage2; //variable constantly monitored by spaceShuttle
+        SpaceShuttle * spaceShuttle; //the spaceShuttle observer
+
     public:
         EOIterator* obsIter;        //Iterator for the engine Observers
         virtual bool fly() = 0;     //Causes the engines to be checked at launch and at the end of flight
         bool landed;
         ///// RSSCreator passed into constructor - RSSCreator constructed in builder
+
+        /*
+            THIS STUFF IS GOING TO BE USED BY THE SPACECRAFT. PLEASE CONSULT BEFORE TOUCHING
+        */
+        
+        void attatch(SpaceShuttle * spaceShuttle);
+        void detatch(); //only one observer. I see no need to accept an argument.
+
+        void notify();
+
+        void setRocketStage2(bool);
+        bool getRocketStage2();
+
+
 };
 #endif
