423de14ba9c1f41adba2a5832da962c776c27891
rayden96 <rayden.logan.viljoen@gmail.com>
2021-11-18 15:58:21 +0200

    Main is started - 11/18 - 15:58

--- Main.cpp
+++ Main.cpp
@@ -3,7 +3,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include 
+//builders
+#include "RocketBuilder.h"
+#include "SatelliteBuilder.h"
+#include "SpacecraftBuilder.h"
+
+//Satellite stuff
+#include "Satellite/Satellite.h"
+#include "Satellite/ChangeTrajectory.h"
+#include "Satellite/CollectionOfSatellites.h"
+#include "Satellite/MaintenanceDepartment.h"
+#include "Satellite/Path.h"
+#include "Satellite/RiskAnalysis.h"
+#include "Satellite/SatelliteIterator.h"
+#include "Satellite/SignalDepartment.h"
+#include "Satellite/ToggleSignal.h"
+#include "Satellite/TrajectoryDepartment.h"
+
+//Rocket Stuff
+#include "Rocket/Engine.h"
+#include "Rocket/EngineObserver.h"
+#include "Rocket/EOIterator.h"
+#include "Rocket/F9Core.h"
+#include "Rocket/Falcon9.h"
+#include "Rocket/Falcon9Factory.h"
+#include "Rocket/FalconHeavy.h"
+#include "Rocket/FalconHeavyFactory.h"
+#include "Rocket/MerlinEngine.h"
+#include "Rocket/Rocket.h"
+#include "Rocket/RocketFactory.h"
+#include "Rocket/RocketFirstStage.h"
+#include "Rocket/RocketIterator.h"
+#include "Rocket/RocketObserver.h"
+#include "Rocket/RocketSecondStage.h"
+#include "Rocket/RocketStage.h"
+#include "Rocket/RSSCreator.h"
+#include "Rocket/StageCreator.h"
+#include "Rocket/VacuumEngine.h"
+
+//Spacecraft stuff
+#include "Spacecraft/Button.h"
+#include "Spacecraft/CrewDragon.h"
+#include "Spacecraft/Deceleration.h"
+#include "Spacecraft/Dock.h"
+#include "Spacecraft/Dragon.h"
+#include "Spacecraft/InternationalSpaceStation.h"
 
 using namespace std;
+
+int main(){//add where you like
+
+    //cout some introduction and explanation
+
+    //Builder to build everything
+
+    //rocket stuff - demonstrate a failure
+    
+    //spacecraft stuff - operate a mission
+    //cout something to screen to show what mission was, how it was done and the cost and objective
+
+    //Rockets detach the satellites
+    //Do full simulation of launching 60 satellites into orbit
+    //monitor them, Perform department operations
+
+    //End off with full explanation of cost vs income
+    //Say whether a real life follow up of the sim would be viable
+    
+
+
+    return 0;
+}
 
