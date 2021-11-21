3663881be98c72bbebf0b9f537274bfd93f7ad27
aryanramnand <aryanramnand@gmail.com>
2021-11-21 11:44:35 +0200

    Changed Satellite Builder

--- SatelliteBuilder.cpp
+++ SatelliteBuilder.cpp
@@ -3,6 +3,6 @@
 CollectionOfSatellites* SatelliteBuilder::buildSatellites(int numSat){
     CollectionOfSatellites* colSat = new CollectionOfSatellites();
     for(int i = 0; i<numSat; i++)
-        colSat->addSatellite(new Satellite(250000,"Sat"+to_string(i))); //Need to explicitly state the cost and name
+        colSat->addSatellite(new Satellite(250000,("Sat"+to_string(i)))); //Need to explicitly state the cost and name
     return colSat;
 }
